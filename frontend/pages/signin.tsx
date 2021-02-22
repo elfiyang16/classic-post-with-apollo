ac82d83d6441435c2c9639105af3d0697ca10659
Elfi Yang <elfiyang16@gmail.com>
2021-02-22 10:51:37 +0000

    feat: add page level user actions

---
+++ frontend/pages/signin.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from "next";
+import { SignIn } from "../components/SignIn";
+
+const LoginPage: NextPage = () => <SignIn />;
+
+export default LoginPage;
---
+++ frontend/pages/signup.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from "next";
+import { SignUp } from "../components/SignUp";
+
+const SignUpPage: NextPage = () => <SignUp />;
+
+export default SignUpPage;

c4cae19537745a12e0226a61ece299d6ba5bac20
Elfi Yang <elfiyang16@gmail.com>
2021-02-22 10:52:27 +0000

    feat: add ambient type support for img/style

---
+++ frontend/types/ambient/images.d.ts
@@ -0,0 +1,19 @@
+declare module "*.svg" {
+  const svg: string;
+  export default svg;
+}
+
+declare module "*.png" {
+  const png: string;
+  export default png;
+}
+
+declare module "*.jpg" {
+  const jpg: string;
+  export default jpg;
+}
+
+declare module "*.gif" {
+  const gif: string;
+  export default gif;
+}
